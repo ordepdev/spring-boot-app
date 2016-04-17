7b21fe6379dd5a96d8c3f2f1f676dd7699ff3ffc
Pedro Tavares <pedrotavares.dev@gmail.com>
2016-04-17 17:02:34 +0100

    more integration tests

--- src/test/java/com/ordepdev/springboot/ShipwreckRepositoryIntegrationTest.java
+++ src/test/java/com/ordepdev/springboot/ShipwreckRepositoryIntegrationTest.java
@@ -23,6 +23,12 @@ public class ShipwreckRepositoryIntegrationTest {
 	private ShipwreckRepository shipwreckRepository;
 	
 	@Test
+	public void save_and_flush_should_create_a_record() {
+		Shipwreck shipwreck = new Shipwreck(1L, "U869", "Description", "Condition", 200, 40, 20, 2000);
+		shipwreckRepository.saveAndFlush(shipwreck);
+	}
+	
+	@Test
 	public void find_all_should_return_results() {
 		List<Shipwreck> shipwrecks = shipwreckRepository.findAll();
 		
